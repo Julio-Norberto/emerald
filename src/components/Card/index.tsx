145ccf2d13556a40c7e73f4926b0959a9ae55653
Julio-Norberto <juliocsscl@gmail.com>
2022-12-11 21:07:42 -0300

    create Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,9 @@
+import './Card.css'
+
+export default function Card() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/WhyUs/index.tsx
@@ -0,0 +1,15 @@
+import './WhyUs.css'
+
+export default function WhyUs() {
+    return (
+        <div className='whyUs-container'>
+            <div className='whyUs-title'>
+
+            </div>
+
+            <div className='whyUs-cards'>
+
+            </div>
+        </div>
+    )
+}
