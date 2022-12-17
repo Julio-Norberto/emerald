ade5955cf84d714b8262ef76a144c8608e418b8d
Julio-Norberto <juliocsscl@gmail.com>
2022-12-17 19:43:48 -0300

    alteration on Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,10 +4,12 @@ import { InstagramLogo, LinkedinLogo, WhatsappLogo } from 'phosphor-react'
 export default function Footer() {
     return (
         <footer className='footer-container'>
-            <p>Emerald 2022. Todos os direitos reservados.</p>
+            <p>Emerald 2022. Todos os direitos reservados. <br /> <a className='ilustrations' href="https://storyset.com/data">Data illustrations by Storyset</a></p>
 
             <div className='social'>
-                <a href=""> <InstagramLogo /> </a>
+                <a target="_blank" href="https://www.instagram.com/julionorberto_s/"> <InstagramLogo size={32} color="#01C38D" /> </a>
+                <a target="_blank" href="https://www.linkedin.com/in/julio-norberto/"> <LinkedinLogo size={32} color="#01C38D" /> </a>
+                <a target="_blank" href=""> <WhatsappLogo size={32} color="#01C38D" /> </a>
             </div>
         </footer>
     )
