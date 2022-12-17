aa9ad833c35ff8be2bcc438f9e4a5b88ba028ee1
Julio-Norberto <juliocsscl@gmail.com>
2022-12-17 19:48:52 -0300

    create page register and create route /register

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,9 @@
+import './Register.css'
+
+export default function Register() {
+    return (
+        <div className='register-container'>
+            <h1>Pagina de registro</h1>
+        </div>
+    )
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes as ReactRoutes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+import Register from '../pages/Register'
 
 export default function Routes() {
     return (
         <ReactRoutes>
             <Route path='/' element={ <Home /> } />
             <Route path='/login' element={ <Login /> } />
+            <Route path='/register' element={ <Register /> } />
         </ReactRoutes>
     )
 }
