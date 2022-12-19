d6d781c936343f84ce18eccc49aad42dcd208440
Julio-Norberto <juliocsscl@gmail.com>
2022-12-19 00:30:15 -0300

    changing footer, adding my name and gthub link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { InstagramLogo, LinkedinLogo, WhatsappLogo } from 'phosphor-react'
 export default function Footer() {
     return (
         <footer className='footer-container'>
-            <p>Emerald 2022. Todos os direitos reservados. <br /> <a className='ilustrations' href="https://storyset.com/data">Data illustrations by Storyset</a></p>
+            <p>Emerald 2022. Feito por <a style={{ color: '#01C38D' }} className='ilustrations' target="_blank" href="https://github.com/Julio-Norberto">Júlio Norberto</a> | <a className='ilustrations' href="https://storyset.com/data">Data illustrations by Storyset</a></p>
 
             <div className='social'>
                 <a target="_blank" href="https://www.instagram.com/julionorberto_s/"> <InstagramLogo size={32} color="#01C38D" /> </a>
