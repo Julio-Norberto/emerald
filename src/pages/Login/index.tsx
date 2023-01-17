14de92ee4f179a23fa03ee418745d604be992c0f
Julio-Norberto <juliocsscl@gmail.com>
2023-01-16 23:36:18 -0300

    feat: add dotenv package and dotenv variables

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,26 +1,45 @@
-import './Login.css'
+import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../services/firebaseConnection.js'
+import { useNavigate } from 'react-router-dom'
 import { Envelope, Key } from 'phosphor-react'
+import './Login.css'
 
 export default function Login() {
-    return (
-        <div className='login-container'>
-            <form className='form-card' action="/login" method='post'>
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const navigate = useNavigate()
 
-                <div className='login-title-div'>
-                    <h2 className='login-title'>Faça Login e aproveite o Emerald!</h2>
-                </div>
-                
-                <div className='div-input'>
-                    <label className='align'>Seu e-mail <Envelope style={{ marginLeft: 10 }} /> </label>
-                    <input type="text" name="email" id="email" placeholder="Digite o seu e-mail..." />
-                </div>
+  // async function handleLogin() {
+  //   if(email && password) {
+  //     await signInWithEmailAndPassword(auth, email, password)
+  //       .then(() => {
+  //         alert('Usuário logado com sucesso!')
+  //         navigate('/dashboard')
+  //       })
+  //       .catch((err) => console.log(err))
+  //   }
+  // }
 
-                <div className='div-input'>
-                    <label className='align'>Sua senha <Key style={{ marginLeft: 10 }} /> </label>
-                    <input type="password" name="password" id="password" placeholder="Digite sua senha..." />
-                </div>
-                <input className='btn-register-login' type="submit" value="Login" />
-            </form>
+  return (
+    <div className='login-container'>
+      <form className='form-card'>
+
+        <div className='login-title-div'>
+            <h2 className='login-title'>Faça Login e aproveite o Emerald!</h2>
         </div>
+
+        <div className='div-input'>
+            <label className='align'>Seu e-mail <Envelope style={{ marginLeft: 10 }} /> </label>
+            <input onChange={(e: any) => setEmail(e.target.value)} type="text" name="email" id="email" placeholder="Digite o seu e-mail..." />
+        </div>
+
+        <div className='div-input'>
+            <label className='align'>Sua senha <Key style={{ marginLeft: 10 }} /> </label>
+            <input onChange={(e: any) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="Digite sua senha..." />
+        </div>
+        <input className='btn-register-login' type="button" value="Login" />
+      </form>
+    </div>
     )
 }
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,34 +1,61 @@
-import './Register.css'
+import { useState } from 'react'
 import { UserCircle, Envelope, Key } from 'phosphor-react'
+import { auth } from '../../services/firebaseConnection.js'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom'
+import './Register.css'
 
 export default function Register() {
-    return (
-        <div className='register-container'>
-            <form className='form-card' action="/register" method='post'>
-                <h2>Registre-se na nossa plataforma!</h2>
-                <div className='div-input'>
-                    <label className='align'>Seu nome <UserCircle size={20} style={{ marginLeft: 10 }} /> </label>
-                    <input type="text" name="name" id="name" placeholder='Digite o seu nome...' />
-                </div>
 
-                <div className='div-input'>
-                    <label className='align'>Seu e-mail <Envelope style={{ marginLeft: 10 }} /> </label>
-                    <input type="text" name="email" id="email" placeholder="Digite o seu e-mail..." />
-                </div>
+  const [email, setEmail] = useState()
+  const [name, setName] = useState()
+  const [password, setPassword] = useState()
+  const [confirmPass, setConfirmPass] = useState()
+  const navigate = useNavigate()
 
-                <div className='div-input'>
-                    <label className='align'>Sua senha <Key style={{ marginLeft: 10 }} /> </label>
-                    <input type="password" name="password" id="password" placeholder="Digite sua senha..." />
-                </div>
+  // async function HandleRegister() {
+  //   if(email && password) {
+  //     if(password === confirmPass) {
+  //       await createUserWithEmailAndPassword(auth, email, password)
+  //         .then(() => {
+  //           alert('Usuário criado com sucesso!')
+  //           navigate('/dashboard')
+  //         }).catch((err) => console.log(err))
+  //     } else {
+  //       alert('As senhas não coincidem')
+  //     }
+  //   } else {
+  //     alert('Preencha todos os campos')
+  //   }
+  // }
 
-                <div className='div-input'>
-                    <label className='align'>Confirme sua senha <Key style={{ marginLeft: 10 }} /> </label>
-                    <input type="password" name="password" id="password" placeholder="Digite novamente sua senha..." />
-                </div>
-                <input className='btn-register' type="submit" value="Registrar" />
+  return (
+      <div className='register-container'>
+          <form className='form-card'>
+              <h2>Registre-se na nossa plataforma!</h2>
+              <div className='div-input'>
+                  <label className='align'>Seu nome <UserCircle size={20} style={{ marginLeft: 10 }} /> </label>
+                  <input onChange={(e: any) => setName(e.target.value)} type="text" name="name" id="name" placeholder='Digite o seu nome...' />
+              </div>
 
-                <p className='login-link-p'>Já possui conta? <a className='login-link' href="/login">Fazer Login</a> </p>
-            </form>
-        </div>
-    )
+              <div className='div-input'>
+                  <label className='align'>Seu e-mail <Envelope style={{ marginLeft: 10 }} /> </label>
+                  <input onChange={(e: any) => setEmail(e.target.value)} type="text" name="email" id="email" placeholder="Digite o seu e-mail..." />
+              </div>
+
+              <div className='div-input'>
+                  <label className='align'>Sua senha <Key style={{ marginLeft: 10 }} /> </label>
+                  <input onChange={(e: any) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="Digite sua senha..." />
+              </div>
+
+              <div className='div-input'>
+                  <label className='align'>Confirme sua senha <Key style={{ marginLeft: 10 }} /> </label>
+                  <input onChange={(e: any) => setConfirmPass(e.target.value)} type="password" placeholder="Digite novamente sua senha..." />
+              </div>
+              <input className='btn-register' value="Registrar" type='button' />
+
+              <p className='login-link-p'>Já possui conta? <a className='login-link' href="/login">Fazer Login</a> </p>
+          </form>
+      </div>
+  )
 }
