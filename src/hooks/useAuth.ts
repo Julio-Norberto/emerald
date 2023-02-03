18a5a6770bfc824670f4480cb736d6ea91ab3189
Julio-Norberto <juliocsscl@gmail.com>
2023-02-03 11:02:03 -0300

    feat: create hook useAuth and function register

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,1 +1,31 @@
-export const useAuth = () => {}
+import api from '../utils/api'
+import { useState, useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+
+interface IUser {
+  name: string,
+  login: string,
+  email: string,
+  password: string,
+}
+
+export const useAuth = () => {
+
+  const register = async(user: IUser) => {
+    try {
+
+      const data = await api.post('/register', user)
+        .then((response) => {
+          return response.data
+        })
+
+        console.log(data)
+
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  return { register }
+
+}
