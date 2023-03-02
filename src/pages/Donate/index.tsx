b60211a91141b2d014b1d9d11b0d3244066c9fb9
Julio-Norberto <juliocsscl@gmail.com>
2023-03-02 13:47:12 -0300

    feat: configuring responsivity of donate page

--- src/pages/Donate/index.tsx
+++ src/pages/Donate/index.tsx
@@ -1,7 +1,34 @@
+import Menu from "../../components/Menu"
+import Footer from "../../components/Footer"
+
+import developer from '../../assets/developer.svg'
+import qrcode from '../../assets/qrcode.png'
+
+import './Donate.css'
+
 export default function Donate() {
   return (
-    <div>
-      Ola
+    <div className="donate-container">
+      <Menu />
+      <div className="donate-content">
+
+        <div className="donate-text">
+          <div className="donate-text-infos">
+            <h1>Quer contruibuir com o projeto?</h1>
+            <p>Sinta-se a vontade para fazer uma contruibuição para o projeto do Emerald. O Emerald é um sistema de controle financeiro totalmente gratuito, se você gosta de usar ele e quer contruibuir com o projeto, por favor sinta-se a vontade para fazer uma doação</p>
+          </div>
+
+          <div>
+            <img className="qrcode" src={qrcode} alt="" />
+          </div>
+        </div>
+
+        <div className="donate-img">
+          <img src={developer} alt="" />
+        </div>
+
+      </div>
+      <Footer />
     </div>
   )
 }
